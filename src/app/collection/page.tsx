e866361ba71fc23a5058ad8bc21dd47d6f782dc1
ja cam <jacam666@example.com>
2025-04-09 12:30:11 +0100

    added styling to headings on homepage and created collection page

---
+++ src/app/collection/page.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export default function Collection() {
+    return (
+        <div className="min-h-screen bg-gradient-to-br from-[#0f0e0e] to-[#454645]">
+            <div className="flex flex-col text-center items-center py-4">
+                <h1 className="font-serif animated-gradient text-5xl sm:text-8xl filter drop-shadow-lg">
+                    Caidees Funko Pops!
+                </h1>
+                <p className="font-sans text-3xl sm:text-4l mx-4 text-white pt-4 filter drop-shadow-lg">
+                    A collection of Funko Pops curated by Caidee.
+                </p>
+            </div>
+            <Link href="/">
+                <button className="flex mx-auto bg-gradient-to-br from-[#0f0e0e] to-[#454645] text-white font-bold py-2 px-4 rounded-full mt-4 hover:bg-gradient-to-bl transition duration-300 ease-in-out">
+                    Take Me Home
+                </button>
+            </Link>
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Oi } from "next/font/google";
 import Image from "next/image";
+import Link from "next/link";
 
 const oi = Oi({
   weight: '400',
@@ -9,13 +10,13 @@ const oi = Oi({
 
 export default function Home() {
   return (
-    <div className=" min-h-screen bg-gradient-to-br from-[#f7cc40] to-[#90ee6b]">
+    <div className=" min-h-screen bg-gradient-to-br from-[#0f0e0e] to-[#454645]">
       <div className="">
         <div className="  flex flex-col text-center items-center py-4 ">
-          <h1 className="font-serif text-5xl sm:text-8xl  text-gray-700 filter drop-shadow-lg">
+          <h1 className="font-serif animated-gradient text-5xl sm:text-8xl filter drop-shadow-lg">
             Caidees Funko Pops!
           </h1>
-          <p className="font-mono text-3xl sm:text-4l mx-4 text-gray-900 pt-4 filter drop-shadow-lg">
+          <p className="font-sans text-3xl sm:text-4l mx-4 text-white pt-4 filter drop-shadow-lg">
             A collection of Funko Pops curated by Caidee.
           </p>
         </div>
@@ -30,10 +31,15 @@ export default function Home() {
           />
         </div>
         <div className="flex flex-col items-center justify-center ">
-          <p className="font-serif text-3xl text-center sm:mx-auto px-4  text-gray-700 mt-4">
+          <p className="font-serif animated-gradient-alt text-3xl text-center sm:mx-auto px-4  text-white mt-4">
             Explore the collection and find your favorite Funko Pops!
           </p>
         </div>
+        <Link href="/collection">
+          <button className="flex mx-auto bg-gradient-to-br from-[#0f0e0e] to-[#454645] text-white font-bold py-2 px-4 rounded-full mt-4 hover:bg-gradient-to-bl transition duration-300 ease-in-out">
+            View Collection
+          </button>
+        </Link>
         <div className="flex flex-col items-center justify-center">
           <Image
             src="/images/body-2-2.png"
